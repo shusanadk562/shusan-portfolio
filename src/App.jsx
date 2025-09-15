4ae1e54eb594f2832f61be6a1cc14da98b15a1a1
Shusan Adhikari <Shusanadk56.npofficial@gmail.com>
2025-09-15 12:34:23 +0545

    Add web app manifest

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
-import { HashRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
+    <>
       <Home />
       <Analytics />
-    </Router>
+    </>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '//',
-});
+  base: '/shusan-portfolio/', // 👈 must start and end with a slash
+})
