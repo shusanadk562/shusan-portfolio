582663bfd8464370a867d7e9913b1f3e57cfc636
Shusan Adhikari <Shusanadk56.npofficial@gmail.com>
2026-01-10 15:50:39 +0545

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/shusan-portfolio/', // 👈 must start and end with a slash
-})
