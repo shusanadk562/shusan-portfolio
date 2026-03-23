be67b6e987bfcbc785ce43da7aa080024dac7508
shusanadk562 <144600882+shusanadk562@users.noreply.github.com>
2026-03-23 14:04:10 +0545

    Update vite.config.js
    changing code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/http://www.shusanadhikari.com.np//', // 👈 must start and end with a slash
+  base: '/',   // ✅ CORRECT for custom domain
 })
